9d998d779d554de586c17baf11ea42e391a749e3
Aditya Jagtap <adityajagtap2774@gmail.com>
2026-08-01 12:07:08 +0530

    Added Initial Groovy Code for Customer Service

---
+++ customer/src/main/groovy/com/policymesh/customer/CustomerApplication.groovy
@@ -0,0 +1,13 @@
+package com.policymesh.customer
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CustomerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(CustomerApplication, args)
+	}
+
+}
